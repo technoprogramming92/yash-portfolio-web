89cac3649ff0b785fe1c30f0dd542693dba9e2cd
Rachit Trivedi <114496132+technoprogramming92@users.noreply.github.com>
2024-12-19 14:06:22 +0530

    completed-till-020004-saumya

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,11 +4,12 @@ function Skills() {
   return (
     <>
     <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
-    <div className='w-full h-screen relative items-center justify-center rounded-full bg-circularLight'>
-        <div className='flex items-center justify-center rounded-full font-semibold bg-[#f5f5f5] text-light'>
-            Web
-        </div>
+    <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight'>
+    <div className='flex items-center justify-center rounded-full font-semibold bg-[#f5f5f5] text-dark p-8 shadow-lg'>
+        Web
     </div>
+</div>
+
     </>
   )
 }
